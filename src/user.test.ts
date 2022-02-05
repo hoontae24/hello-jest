a58e406e30ae84c6240a8fdbaa53485ec2d4d989
hoontae24 <kadr8941@gmail.com>
2022-02-05 20:40:11 +0900

    async using callback

--- src/user.test.ts
+++ src/user.test.ts
@@ -1,4 +1,8 @@
-import { makeUser } from "./user";
+import {
+  getNameUsingCallback,
+  getNameUsingCallbackOnError,
+  makeUser,
+} from "./user";
 
 test("Make a user object from name and age.(toEqual)", () => {
   expect(makeUser("John", 30)).toEqual({ name: "John", age: 30 });
@@ -9,5 +13,24 @@ test("Make a user object from name and age.(toStrictEqual)", () => {
     name: "John",
     age: 30,
     gender: undefined,
+  });
+});
+
+test("Get user's name using callback", (done) => {
+  const user = makeUser("John", 30);
+  getNameUsingCallback(user, (name) => {
+    expect(name).toBe("John");
+    done();
+  });
+});
+
+test("Get user's name using callback on error", (done) => {
+  const user = makeUser("John", 30);
+  getNameUsingCallbackOnError(user, (error, name) => {
+    expect(() => {
+      throw error;
+    }).toThrow("Error");
+    expect(name).toBeUndefined();
+    done();
   });
 });
--- src/user.ts
+++ src/user.ts
@@ -7,3 +7,21 @@ export interface User {
 export const makeUser = (name: string, age: number): User => {
   return { name, age, gender: undefined };
 };
+
+export const getNameUsingCallback = (
+  user: User,
+  callback: (name: string) => void
+) => {
+  setTimeout(() => {
+    callback(user.name);
+  }, 1000);
+};
+
+export const getNameUsingCallbackOnError = (
+  user: User,
+  callback: (error: Error, name?: string) => void
+) => {
+  setTimeout(() => {
+    callback(new Error("Error"));
+  }, 1000);
+};
